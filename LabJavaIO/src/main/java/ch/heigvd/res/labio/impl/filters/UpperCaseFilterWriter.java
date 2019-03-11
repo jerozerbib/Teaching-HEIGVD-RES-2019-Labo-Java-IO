72e6fe2a8775c9df67bcab641d5696258a2b51d9
jeremy <jerozerbib>
2019-03-11 10:27:33 +0100

    UpperCase works according to test

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -4,6 +4,8 @@ import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
 
+import static java.lang.Character.toUpperCase;
+
 /**
  *
  * @author Olivier Liechti
@@ -16,17 +18,22 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = 0; i < cbuf.length; ++i){
+      cbuf[i] = toUpperCase(cbuf[i]);
+    }
+
+    super.write(cbuf, off, len);
+
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(toUpperCase(c));
   }
 
 }
