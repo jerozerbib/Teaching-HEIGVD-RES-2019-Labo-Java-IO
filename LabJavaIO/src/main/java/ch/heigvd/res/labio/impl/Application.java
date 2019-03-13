14550bb1b6a9b3bc89126eb48f830599a488f809
jeremy <jerozerbib>
2019-03-13 13:17:32 +0100

    Tout passe au JUnit

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -10,7 +10,6 @@ import ch.heigvd.res.labio.quotes.QuoteClient;
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,19 +24,19 @@ public class Application implements IApplication {
    * to where the Java application is invoked.
    */
   public static String WORKSPACE_DIRECTORY = "./workspace/quotes";
-  
+
   private static final Logger LOG = Logger.getLogger(Application.class.getName());
-  
+
   public static void main(String[] args) {
-    
+
     /*
      * I prefer to have LOG output on a single line, it's easier to read. Being able
      * to change the formatting of console outputs is one of the reasons why it is
      * better to use a Logger rather than using System.out.println
      */
     System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
-    
-       
+
+
     int numberOfQuotes = 0;
     try {
       numberOfQuotes = Integer.parseInt(args[0]);
@@ -45,32 +44,32 @@ public class Application implements IApplication {
       System.err.println("The command accepts a single numeric argument (number of quotes to fetch)");
       System.exit(-1);
     }
-        
+
     Application app = new Application();
     try {
       /*
        * Step 1 : clear the output directory
        */
       app.clearOutputDirectory();
-      
+
       /*
        * Step 2 : use the QuotesClient to fetch quotes; store each quote in a file
        */
       app.fetchAndStoreQuotes(numberOfQuotes);
-      
+
       /*
        * Step 3 : use a file explorer to traverse the file system; print the name of each directory and file
        */
       Writer writer = new StringWriter(); // we create a special writer that will send characters into a string (memory)
       app.printFileNames(writer);         // we hand over this writer to the printFileNames method
       LOG.info(writer.toString());       // we dump the whole result on the console
-      
+
       /*
        * Step 4 : process the quote files, by applying 2 transformations to their content
        *          (convert to uppercase and add line numbers)
        */
       app.processQuoteFiles();
-      
+
     } catch (IOException ex) {
       LOG.log(Level.SEVERE, "Could not fetch quotes. {0}", ex.getMessage());
       ex.printStackTrace();
@@ -92,31 +91,31 @@ public class Application implements IApplication {
       }
     }
   }
-  
+
   /**
    * This method deletes the WORKSPACE_DIRECTORY and its content. It uses the
    * apache commons-io library. You should call this method in the main method.
-   * 
-   * @throws IOException 
+   *
+   * @throws IOException
    */
   void clearOutputDirectory() throws IOException {
-    FileUtils.deleteDirectory(new File(WORKSPACE_DIRECTORY));    
+    FileUtils.deleteDirectory(new File(WORKSPACE_DIRECTORY));
   }
 
   /**
    * This method stores the content of a quote in the local file system. It has
-   * 2 responsibilities: 
-   * 
+   * 2 responsibilities:
+   *
    * - with quote.getTags(), it gets a list of tags and uses
    *   it to create sub-folders (for instance, if a quote has three tags "A", "B" and
    *   "C", it will be stored in /quotes/A/B/C/quotes-n.utf8.
-   * 
+   *
    * - with quote.getQuote(), it has access to the text of the quote. It stores
    *   this text in UTF-8 file.
-   * 
+   *
    * @param quote the quote object, with tags and text
    * @param filename the name of the file to create and where to store the quote text
-   * @throws IOException 
+   * @throws IOException
    */
   void storeQuote(Quote quote, String filename) throws IOException {
     StringBuilder path = new StringBuilder(WORKSPACE_DIRECTORY);
@@ -126,16 +125,16 @@ public class Application implements IApplication {
       path.append("/").append(tag);
     }
 
-    path.append("/").append(filename).append(".utf-8");
+    path.append("/").append(filename).append(".utf8");
 
     File newF = new File(path.toString());
     newF.getParentFile().mkdirs();
 
-    Writer writer = new OutputStreamWriter(new FileOutputStream(newF), StandardCharsets.UTF_8);
+    Writer writer = new OutputStreamWriter(new FileOutputStream(newF), "UTF-8");
     writer.write(quote.getQuote());
     writer.close();
   }
-  
+
   /**
    * This method uses a IFileExplorer to explore the file system and prints the name of each
    * encountered file and directory.
@@ -162,7 +161,7 @@ public class Application implements IApplication {
   @Override
   public void processQuoteFiles() throws IOException {
     IFileExplorer explorer = new DFSFileExplorer();
-    explorer.explore(new File(WORKSPACE_DIRECTORY), new CompleteFileTransformer());    
+    explorer.explore(new File(WORKSPACE_DIRECTORY), new CompleteFileTransformer());
   }
 
 }
